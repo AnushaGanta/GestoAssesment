a8eb37d6a8e4f076586f44691a68b0e5d969344f
Narentharaa <naren180891@gmail.com>
2016-04-01 20:02:06 -0500

    Finished this page

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,7 +1,7 @@
 angular.module('foodController', [])
 
 // inject the Food service factory into our controller
-.controller('mainController', ['$scope', '$http', 'Foods', function($scope, $http, Foods) {
+.controller('mainController', ['$scope', '$http', 'Foods', 'alertify', function($scope, $http, Foods, alertify) {
 	$scope.formData = {};
 	$scope.loading = true;
 	$scope.total = 0;
@@ -45,16 +45,14 @@ angular.module('foodController', [])
 
 			// call the create function from our service (returns a promise object)
 			Foods.create($scope.formData)
-
 			// if successful creation, call our get function to get all the new foods
 			.success(function(data) {
 				Foods.total()
 					.success(function(data) {
-						console.log("Total = " + data);
 						$scope.total = data;
 						$scope.loading = false;
+						alertify.success('Added new item');
 					});
-
 				$scope.formData = {}; // clear the form so our user is ready to enter another
 				$scope.foods = data; // assign our new list of foods
 			});
@@ -67,17 +65,20 @@ angular.module('foodController', [])
 	$scope.deleteFood = function(id) {
 		$scope.loading = true;
 
-		Foods.delete(id)
-			// if successful creation, call our get function to get all the new foods
-			.success(function(data) {
-				Foods.total()
-					.success(function(data) {
-						$scope.total = data;
-						$scope.loading = false;
-					});
+		alertify.confirm('Delete item?', function () {
+			Foods.delete(id)
+				// if successful creation, call our get function to get all the new foods
+				.success(function(data) {
+					Foods.total()
+						.success(function(data) {
+							$scope.total = data;
+							$scope.loading = false;
+							alertify.error('Successfully removed item');
+						});
 
-				$scope.foods = data; // assign our new list of foods
-			});
+					$scope.foods = data; // assign our new list of foods
+				});
+		});
 	};
 
 	$scope.toggle = function(id) {
--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,1 @@
-angular.module('gestoFood', ['foodController', 'foodService', 'ng-backstretch']);
+angular.module('gestoFood', ['foodController', 'foodService', 'ng-backstretch', 'ngAlertify']);
